dbc5793ed677c4f7071d84254d0590ea237c7852
tonlen <1750041@qq.com>
2016-06-15 15:16:07 +0800

    Inital commit

---
+++ TPMultiImageDownloader.h
@@ -0,0 +1,17 @@
+//
+//  TPMultiImageDownloader.h
+//  ChangNet
+//
+//  Created by HZ on 16/6/15.
+//  Copyright © 2016年 SCHH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TPMultiImageDownloader : NSObject
++ (instancetype)shareDownloader;
+
+- (void)downloadImagesWithURLs:(NSArray *)urls
+                       success:(void (^)(NSArray<UIImage *> *images))success
+                       failure:(void (^)(NSError *error))failure;
+@end
---
+++ TPMultiImageDownloader.m
@@ -0,0 +1,164 @@
+//
+//  TPMultiImageDownloader.m
+//  ChangNet
+//
+//  Created by HZ on 16/6/15.
+//  Copyright © 2016年 SCHH. All rights reserved.
+//
+
+#import "TPMultiImageDownloader.h"
+#import <objc/runtime.h>
+#import "AFImageDownloader.h"
+
+@interface TPMultiImageDownloader()
+@property (nonatomic, strong) NSMutableArray<AFImageDownloadReceipt *> *activeImageDownloadReceipts;
+@property (nonatomic, strong) NSMutableArray *downloadImages;
+@end
+
+@implementation TPMultiImageDownloader
+
++ (instancetype)shareDownloader
+{
+    static TPMultiImageDownloader *shareDownloader = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        shareDownloader = [[self alloc] init];
+    });
+    return shareDownloader;
+}
+
++ (AFImageDownloader *)sharedImageDownloader {
+    
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wgnu"
+    return objc_getAssociatedObject(self, @selector(sharedImageDownloader)) ?: [AFImageDownloader defaultInstance];
+#pragma clang diagnostic pop
+}
+
++ (void)setSharedImageDownloader:(AFImageDownloader *)imageDownloader {
+    objc_setAssociatedObject(self, @selector(sharedImageDownloader), imageDownloader, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        self.downloadImages = [NSMutableArray array];
+        self.activeImageDownloadReceipts = [NSMutableArray array];
+    }
+    return self;
+}
+
+- (void)downloadImagesWithURLs:(NSArray *)urls
+                      success:(void (^)(NSArray<UIImage *> *images))success
+                      failure:(void (^)(NSError *error))failure
+{
+    __weak __typeof(&*self)weakSelf = self;
+    [urls enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        [self downloadImageURL:obj success:^(UIImage *image) {
+            [weakSelf.downloadImages addObject:image];
+            // download completion
+            if (weakSelf.downloadImages.count >= urls.count) {
+                if (success) {
+                    success(self.downloadImages);
+                }
+            }
+        } failure:^(NSError *error) {
+            if (failure) {
+                failure(error);
+            }
+        }];
+    }];
+}
+
+- (void)downloadImageURL:(id)url
+              success:(void (^)(UIImage *image))success
+              failure:(void (^)(NSError *error))failure
+{
+    if ([url isKindOfClass:[NSString class]]) {
+        url = [NSURL URLWithString:url];
+    }
+    if (![url isKindOfClass:[NSURL class]]) {
+        return;
+    }
+    NSMutableURLRequest *urlRequest = [NSMutableURLRequest requestWithURL:url];
+    [urlRequest addValue:@"image/*" forHTTPHeaderField:@"Accept"];
+    if ([urlRequest URL] == nil) {
+        return;
+    }
+    
+    if ([self isActiveTaskURLEqualToURLRequest:urlRequest]){
+        return;
+    }
+    
+    AFImageDownloader *downloader = [[self class] sharedImageDownloader];
+    id <AFImageRequestCache> imageCache = downloader.imageCache;
+    
+    __block UIImage *image;
+    //Use the image from the image cache if it exists
+    UIImage *cachedImage = [imageCache imageforRequest:urlRequest withAdditionalIdentifier:nil];
+    if (cachedImage) {
+        if (success) {
+            success(cachedImage);
+        }
+    } else {
+        __weak __typeof(&*self)weakSelf = self;
+        NSUUID *downloadID = [NSUUID UUID];
+        AFImageDownloadReceipt *receipt;
+        receipt = [downloader
+                   downloadImageForURLRequest:urlRequest
+                   withReceiptID:downloadID
+                   success:^(NSURLRequest * _Nonnull request, NSHTTPURLResponse * _Nullable response, UIImage * _Nonnull responseObject) {
+                       __strong __typeof(weakSelf)strongSelf = weakSelf;
+                       if ([strongSelf isContainsDownloadID:downloadID]) {
+                           if (success) {
+                               success(responseObject);
+                           } else if(responseObject) {
+                               image = responseObject;
+                           }
+                           [strongSelf clearActiveDownloadInformation:downloadID];
+                       }
+                       
+                   }
+                   failure:^(NSURLRequest * _Nonnull request, NSHTTPURLResponse * _Nullable response, NSError * _Nonnull error) {
+                       __strong __typeof(weakSelf)strongSelf = weakSelf;
+                       if ([strongSelf isContainsDownloadID:downloadID]) {
+                           if (failure) {
+                               failure(error);
+                           }
+                           [strongSelf clearActiveDownloadInformation:downloadID];
+                       }
+                   }];
+        
+        [self.activeImageDownloadReceipts addObject:receipt];
+    }
+}
+
+- (void)clearActiveDownloadInformation:(NSUUID *)downloadID {
+    AFImageDownloadReceipt *willRemoveReceipt = [self isContainsDownloadID:downloadID];
+    if (willRemoveReceipt) [self.activeImageDownloadReceipts removeObject:willRemoveReceipt];
+}
+
+- (BOOL)isActiveTaskURLEqualToURLRequest:(NSURLRequest *)urlRequest {
+    BOOL isActive = NO;
+    for (AFImageDownloadReceipt *receipt in self.activeImageDownloadReceipts) {
+        if ([receipt.task.originalRequest.URL.absoluteString isEqualToString:urlRequest.URL.absoluteString]) {
+            isActive = YES;
+            break;
+        }
+    }
+    return isActive;
+}
+
+- (AFImageDownloadReceipt *)isContainsDownloadID:(NSUUID *)uuid
+{
+    AFImageDownloadReceipt *containedReceipt;
+    for (AFImageDownloadReceipt *receipt in self.activeImageDownloadReceipts) {
+        if ([receipt.receiptID isEqual:uuid]) {
+            containedReceipt = receipt;
+        }
+    }
+    return containedReceipt;
+}
+
+@end
